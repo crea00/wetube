07958e008a84d6cd5dc199c35a6ef6b1fa393d36
Jerome <654321su@gmail.com>
2019-01-06 21:09:23 +0900

    #2.6 Express Core: Middlewares part One

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const handleHome = (req, res) => res.send('Hello from home');
 
 const handleProfile = (req, res) => res.send('You are on my profile');
 
+const betweenHome = (req, res, next) => {
+  console.log('Between');
+  next();
+};
+
+// This is a middleware, The order really matters
+app.use(betweenHome);
+
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
