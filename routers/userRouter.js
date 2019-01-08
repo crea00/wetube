00cf21e3c86946206db5ab588a89c0b6bc083eda
Jerome <654321su@gmail.com>
2019-01-08 09:49:54 +0900

    Create pug files

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,7 +9,6 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.userdetail, userDetail);
 userRouter.get(routes.editprofile, editProfiile);
 userRouter.get(routes.changePassword, changePassword);
