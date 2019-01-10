4b9fa15b91452e4780de11a19d3ae37ee0984aea
Jerome <654321su@gmail.com>
2019-01-10 10:47:43 +0900

    Fix the misspelled
     - Change the 'editProfiile' to 'editProfile'
     - Fix the words in camel case which is not

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,14 +2,14 @@ import express from 'express';
 import routes from '../routes';
 import { 
   userDetail,
-  editProfiile,
+  editProfile,
   changePassword,
 } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userdetail, userDetail);
-userRouter.get(routes.editprofile, editProfiile);
+userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
